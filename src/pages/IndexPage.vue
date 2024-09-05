a1448cbdba5cfa0f3b402e02e927986fbd39d424
368 ภีรภัทร์ <138644393+pexrapat@users.noreply.github.com>
2024-09-05 15:39:06 +0700

    Uni_368

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
+
---
+++ src/pages/InputPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="secondName" v-model="secondName" />
+      <input type="text" id="Nickname" v-model="Nickname" />
+      <input type="text" id="StudentCode" v-model="StudentCode" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+      firstName: 'Peerapat',
+      secondName: 'Khodkaew',
+      Nickname: 'Bas',
+      StudentCode: '6604101368',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
